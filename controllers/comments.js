a2995cbc5f10c9147c0e59a20fdb445be78977b9
SunilDeveloper7 <sunnil474@gmail.com>
2021-03-24 15:55:14 -0700

    install packages aslo worked on controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -19,38 +19,67 @@ router.get('/', async (req, res) => {
 
 
 router.post('/', async (req, res) => {
-    try {
-        const comment = await Comment.create({})
-        console.log(comment)
-        res.json( { comment })
-    } catch (err) {
-        res.json({err})
-        console.log(err)
-    }
+    const newComment = await Comment.create({
+        content: req.body.content
+    })
+    res.json(newComment)
 })
 
 router.put('/:id', async (req, res) => {
-    try {
-        const comment = await Comment.updateOne({})
-        console.log(comment)
-        res.json( { comment })
-    } catch (err) {
-        res.json({err})
-        console.log(err)
-    }
+    const updateComment = await Comment.findByIdAndUpdate(req.params.id, {
+        content: req.body.content
+    })
+    res.json(updateComment)
 })
 
 
 router.delete('/:id', async (req, res) => {
-    try {
-        const comment = await Comment.deleteOne({})
-        console.log(comment)
-        res.json( { comment })
-    } catch (err) {
-        res.json({err})
-        console.log(err)
-    }
+    const deleteComment = await Comment.findByIdAndDelete(req.params.id)
+    res.json(deleteComment)
 })
 
-
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+// // Read (Index)
+// router.get('/', async (req, res) => {
+    //     const allComment = await Comment.find({})
+//     res.json(allComment)
+// })
+
+
+// Update
+// router.put('/:id/comment_content', async (req, res) => {
+//     try {
+//         const comment = await Comment.updateOne({})
+//         console.log(comment)
+//         res.json( { comment })
+//     } catch (err) {
+//         res.json({err})
+//         console.log(err)
+//     }
+// })
+
+
+// router.delete('/:id/comment_content', async (req, res) => {
+//     try {
+//         const comment = await Comment.delete({})
+//         console.log(comment)
+//         res.json( { comment })
+//     } catch (err) {
+//         res.json({err})
+//         console.log(err)
+//     }
+// })
+// Delete
+
+
